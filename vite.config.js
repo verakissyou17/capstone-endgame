d5ba063df82874bc556e00fc5f6436430d4c8140
verakissyou <veronicaciuca17@gmail.com>
2025-04-29 12:27:20 +0300

    Edit files

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'index.html', // Use index.html as the fallback
+          dest: '.',         // Copy it as 404.html in the root
+          rename: '404.html',
+        },
+      ],
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
         main: 'index.html',
-        fallback: '404.html'
       },
     },
   },
 });
+
 
