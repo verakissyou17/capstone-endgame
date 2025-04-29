277f11303e6906bf0602ed52b70284e5efb38a63
verakissyou <veronicaciuca17@gmail.com>
2025-04-29 12:36:29 +0300

    Update vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'index.html', // Use index.html as the fallback
-          dest: '.',         // Copy it as 404.html in the root
-          rename: '404.html',
-        },
-      ],
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      },
-    },
-  },
+  base: '/capstone-endgame/', // Set the base path for GitHub Pages
+  plugins: [react()],
 });
-
-
